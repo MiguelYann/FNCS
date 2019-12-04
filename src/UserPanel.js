f3c01aaa130e8f1dab1277c1873353dc616393d2
Miguel Yann <tsambamiguelito@gmail.com>
2019-12-04 17:59:36 +0100

    Conversion en JSX

--- src/UserPanel.js
+++ src/UserPanel.js
@@ -2,24 +2,36 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import md5 from 'js-md5';
 
-export default function UserPanel({nom, prenom,email}) {
-    UserPanel.propTypes = {
-        nom: PropTypes.string.isRequired,
-        prenom: PropTypes.string.isRequired,
-        email: PropTypes.string.isRequired,
-    }
+UserPanel.propTypes = {
+    nom: PropTypes.string.isRequired,
+    prenom: PropTypes.string.isRequired,
+    email: PropTypes.string.isRequired,
+}
+UserPanel.defaultProps = {
+    nom: 'Miguel',
+    prenom: 'Yann',
+    email: 'baptiste.lecocq@gmail'}
+export default function UserPanel(props){
     
-    UserPanel.defaultProps = {
-        nom: 'Miguel',
-        prenom: 'Yann',
-        email: 'baptiste.lecocq@gmail'}
+   
+    return (<div>
+        <p>{props.nom}</p>
+        <p>{props.prenom}</p>
+        <p>{props.email}</p>
+        <img src={"https://www.gravatar.com/avatar/"+md5(props.email)} alt="img"/>
 
-        return React.createElement('div',{}, null,
-        React.createElement('p',  {style: { color: 'black'}}, 'First Name: '+ nom),
-        React.createElement('p',  {style: { color: 'red'}}, 'Last Name: '+ prenom),
-        React.createElement('p',  {style: { color: 'brown'}}, 'Email: '+ email),
-        React.createElement('img',  { src :'https://www.gravatar.com/avatar/'+md5(email) }, null),
-        );
-    }
+    </div>)
+}
 
+
+// export default function UserPanel({nom, prenom,email}) {
+   
+
+//         return React.createElement('div',{}, null,
+//         React.createElement('p',  {style: { color: 'black'}}, 'First Name: '+ nom),
+//         React.createElement('p',  {style: { color: 'red'}}, 'Last Name: '+ prenom),
+//         React.createElement('p',  {style: { color: 'brown'}}, 'Email: '+ email),
+//         React.createElement('img',  { src :'https://www.gravatar.com/avatar/'+md5(email) }, null),
+//         );
+//     }
 
--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import  UserPanel  from './UserPanel';
 
-ReactDOM.render(React.createElement(UserPanel, {
-    nom:'miguel',
-    prenom:'yann',
-    email:'tsambamiguelito@gmail.com'
-}),document.getElementById('root'));
+// ReactDOM.render(React.createElement(UserPanel, {
+//     nom:'miguel',
+//     prenom:'yann',
+//     email:'baptiste.lecocq@gmail.com'
+// }),document.getElementById('root'));
 
-    
+    export default function App(){
+      return( 
+      <UserPanel nom="miguel" prenom="yann" email="tsambamiguelito@gmail.com" />
+      )
+    }
+
+    ReactDOM.render(<App/>,document.getElementById('root'))
