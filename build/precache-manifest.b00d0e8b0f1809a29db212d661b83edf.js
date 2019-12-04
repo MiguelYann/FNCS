a5c72fb61c5fbe0c02537dc895be0ad9c537c8e5
Miguel Yann <tsambamiguelito@gmail.com>
2019-12-04 16:45:59 +0100

    Ajout build

---
+++ build/precache-manifest.b00d0e8b0f1809a29db212d661b83edf.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d23f44048b73b277ae549bfd21e5bee",
+    "url": "/FNCS/build/index.html"
+  },
+  {
+    "revision": "a3b8f7f60075f774b61b",
+    "url": "/FNCS/build/static/js/2.6d2b7704.chunk.js"
+  },
+  {
+    "revision": "0ab183692b1677b3a8d2",
+    "url": "/FNCS/build/static/js/main.6df171b6.chunk.js"
+  },
+  {
+    "revision": "f80c12b1e7cfb93c2a2e",
+    "url": "/FNCS/build/static/js/runtime-main.a174259c.js"
+  }
+]);
